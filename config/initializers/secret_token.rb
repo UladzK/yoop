4589fcf4c2309b313883cdc8df60b3a52adaae6f
vova <vladimir.fiction@gmail.com>
2012-10-07 00:28:43 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoop::Application.config.secret_token = '0c8312198444b1d0533d32da003b67f30dd74bfd0b755c27170a107f7dc7b7342e043b0e45bcb207038e84e2608850dbd6d73cb39016835587b3e88373f0034c'
