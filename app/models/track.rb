5236b48eeb186fa2d46605c818b9f97a39cf7f44
vova <vladimir.fiction@gmail.com>
2012-11-15 13:36:25 +0300

    Added validation to artists and tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,9 @@
 class Track < ActiveRecord::Base
   attr_accessible :title, :lyrics, :artist_ids
   has_and_belongs_to_many :artists, :join_table => 'artists_tracks'
+
+  validates_presence_of :title, :on => :create, :message => "cannot be blank"
+  #validates :title, :format => { :with => /\A[a-zA-Z]+\z/,
+  #                               :message => "only letters allowed" }
+  validates_presence_of :artists, :on => :create, :message => "must be selected"
 end
