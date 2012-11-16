2599192f6f485682683de5de7cd1775b0cae59c4
vova <vladimir.fiction@gmail.com>
2012-11-16 21:54:53 +0300

    Added youtube links to tracks

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,6 @@
 module TracksHelper
+  def youtube_id(url)
+    $regexp=/\=([a-zA-Z0-9_-]*)/
+    $youtube_link = "http://www.youtube.com/v/#{$regexp.match(url)[1]}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025172933) do
+ActiveRecord::Schema.define(:version => 20121115110646) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121025172933) do
     t.text     "lyrics"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "url"
   end
 
   create_table "users", :force => true do |t|
